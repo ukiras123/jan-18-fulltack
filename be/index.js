bc533f919a5293b9200f7ee73b7e5a7ca56ba990
Kiran Gautam <ukiras@gmail.com>
2024-01-18 12:52:37 +1100

    remove gitignore files

--- be/index.js
+++ be/index.js
@@ -2,11 +2,14 @@ const express = require('express');
 const { todoRouter } = require('./src/routes/todo');
 // import express from 'express'
 const { mongoConnect } = require('./src/dbConfig/mongoConnect.js')
+const cors = require('cors');
 const app = express();
 mongoConnect();
 const PORT = 3000;
 // Middleware
 app.use(express.json())
+app.use(cors())
+
 app.use("/api/v1/todo", todoRouter)
 
 
--- be/src/dbConfig/mongoConnect.js
+++ be/src/dbConfig/mongoConnect.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const mongoConnect = () => {
     console.log("DB Connected")
-    return mongoose.connect(`mongodb+srv://todouser:Px7icaA3qWgHi17E@todo.sv1imwp.mongodb.net/`)
+    return mongoose.connect(`mongodb+srv://todouser:Px7icaA3qWgHi17E@todo.sv1imwp.mongodb.net/todoapp`)
 }
 
 module.exports = {
